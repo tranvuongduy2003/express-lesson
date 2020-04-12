1fcb6c86c1a0532b4640492612490ca4e1d2e0cc
tran vuong duy <tranvuongduy2003@gmail.com>
2020-04-12 14:33:38 +0700

    Lesson 3 in Express

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var port = 3000;
 app.set('views', './views');
 app.set('view engine', 'pug');
 
+var users = [
+	{ id: 1, name: 'Thinh' },
+	{ id: 2, name: 'Nam' }
+]
+
 app.get('/', function(req, res) {
 	res.render('index', {
 		name: 'ABC'
@@ -14,12 +19,23 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
 	res.render('users/index', {
-		users: [
-			{ id: 1, name: 'Thinh' },
-			{ id: 2, name: 'Nam' }
-		]
+		users: users
 	});
 });
+
+
+app.get('/users/search', function(req, res) { 
+	var q = req.query.q;
+	var matchedUsers = users.filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+	});
+
+	res.render('users/index', {
+		users: matchedUsers,
+		inputValue: q
+	});
+});
+
 
 app.listen(port, function() {
  console.log('Sever listening on port ' + port);
