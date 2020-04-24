1d63201af41b1142212ed2c960a3937f8489c08a
tran vuong duy <tranvuongduy2003@gmail.com>
2020-04-24 17:48:55 +0700

    Lesson 8 in Express

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 var express = require('express');
-var shortid = require('shortid');
-var app = express();
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route');
 
 var port = 3000;
 
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-
-var adapter = new FileSync('db.json');
-var db = low(adapter);
-
-db.defaults({ users: [] })
-  .write();
-
+var app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -25,47 +18,7 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.get('/users', function(req, res) {
-	res.render('users/index', {
-		users: db.get('users').value()
-	});
-});
-
-
-app.get('/users/search', function(req, res) { 
-	var q = req.query.q;
-	var matchedUsers = users.filter(function(user) {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1
-	});
-
-	res.render('users/index', {
-		users: matchedUsers,
-
-		inputValue: q
-	});
-});
-
-
-app.get('/users/create', function(req, res) {
-	res.render('users/create');
-});
-
-app.post('/users/create', function(req, res) {
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write();
-	res.redirect('/users');
-});
-
-app.get('/users/:id', function(req, res) {
-	var id = req.params.id;
-	var user = db.get('users').find({ id: id }).value();
-
-
-
-	res.render('users/view', {
-		user: user
-	});
-});
+app.use('/users', userRoute);
 
 app.listen(port, function() {
  console.log('Sever listening on port ' + port);
